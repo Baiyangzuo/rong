d3f5d520d6996a05a3a958782eeefe3f3404bc2e
gavinning <ningyubo@gmail.com>
2016-08-23 01:29:05 +0800

    v0.1.6 update routes

--- src/main/node/routes/home/index.js
+++ src/main/node/routes/home/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('main/index', { title: 'Express' });
-})
-
-.get('/demo', function(req, res, next) {
     res.render('main/home', { title: 'Express' });
 })
+
+// .get('/demo', function(req, res, next) {
+//     res.render('main/index', { title: 'Express' });
+// })
 
 module.exports = router;
