b8199779547034be19aea6432659a5fef9a44271
gavinning <ningyubo@gmail.com>
2016-09-18 22:24:17 +0800

    Fixbug fix cache maxAge

--- src/main/node/models/system/cache.js
+++ src/main/node/models/system/cache.js
@@ -3,7 +3,7 @@ var options = {
     max: 500,
     length: function (n, key) { return n * 2 + key.length },
     dispose: function (key, n) { n.close() },
-    maxAge: 1000 * 60 * 60 * 24
+    maxAge: 1000 * 60 * 60 * 24 * 365
 }
 var cache = module.exports = LRU(options)
 
