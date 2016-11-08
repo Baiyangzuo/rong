f56d8596a72a0ca0095be05217f2e9ee761c1257
gavinning <ningyubo@gmail.com>
2016-11-08 16:05:59 +0800

    v1.0.2 完善更多功能

--- src/main/node/routes/home/index.js
+++ src/main/node/routes/home/index.js
@@ -6,6 +6,12 @@ var public = {
     version: 'v1'
 };
 
+var auto = extend(require('../../config/v1/auto'), public);
+var cards = extend(require('../../config/v1/cards'), public);
+var house = extend(require('../../config/v1/house'), public);
+var credit = extend(require('../../config/v1/credit'), public);
+var retired = extend(require('../../config/v1/retired'), public);
+
 
 router.get('/m', function(req, res, next) {
     res.render('imp/home', { isMobile: true });
@@ -16,15 +22,14 @@ router.get('/m', function(req, res, next) {
 router
 
 .get('/', (req, res) => {
-    res.render('imp1/home', { isMobile: req.env.isMobile, version: 'v1' })
+    res.render('imp1/home', { version: 'v1', id: 'home' })
 })
 
 .get('/apply', (req, res) => res.render('imp1/apply', public))
-.get('/loan/auto', (req, res) => res.render('imp1/detail', public))
-.get('/loan/cards', (req, res) => res.render('imp1/detail', public))
-.get('/loan/apply', (req, res) => res.render('imp1/apply', public))
-.get('/loan/house', (req, res) => res.render('imp1/detail', public))
-.get('/loan/credit', (req, res) => res.render('imp1/detail', public))
-.get('/loan/retired', (req, res) => res.render('imp1/detail', public))
+.get('/loan/auto', (req, res) => res.render('imp1/detail', auto))
+.get('/loan/cards', (req, res) => res.render('imp1/detail', cards))
+.get('/loan/house', (req, res) => res.render('imp1/detail', house))
+.get('/loan/credit', (req, res) => res.render('imp1/detail', credit))
+.get('/loan/retired', (req, res) => res.render('imp1/detail', retired))
 
 module.exports = router;
