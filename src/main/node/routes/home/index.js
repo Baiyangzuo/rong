f305a69f6364cefa5c9dc5d70803489fd6f1d775
gavinning <ningyubo@gmail.com>
2016-11-14 00:30:09 +0800

    v1.0.6 add article list

--- src/main/node/routes/home/index.js
+++ src/main/node/routes/home/index.js
@@ -1,6 +1,7 @@
-var extend = require('aimee-extend');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
+var extend = require('aimee-extend');
 var nav = require(paths.nav);
 var public = {
     version: 'v1'
@@ -17,6 +18,9 @@ var pingan = extend(require('../../config/v1/pingan'), public);
 var yiche = extend(require('../../config/v1/yiche'), public);
 var rongyi = extend(require('../../config/v1/rongyi'), public);
 
+// Article list
+var articleList = require('../../config/v1/articles');
+
 
 router.get('/m', function(req, res, next) {
     res.render('imp/home', { isMobile: true });
@@ -27,7 +31,7 @@ router.get('/m', function(req, res, next) {
 router
 
 .get('/', (req, res) => {
-    res.render('imp1/home', { version: 'v1', id: 'home' })
+    res.render('imp1/home', { version: 'v1', id: 'home', list: articleList })
 })
 
 .get('/apply', (req, res) => res.render('imp1/apply', public))
@@ -41,5 +45,21 @@ router
 .get('/loan/pingan', (req, res) => res.render('imp1/detail', pingan))
 .get('/loan/yiche', (req, res) => res.render('imp1/detail', yiche))
 .get('/loan/rongyi', (req, res) => res.render('imp1/detail', rongyi))
+
+.get('/article/:id', (req, res) => {
+    let data;
+    console.log(path.join('../../config/arts', req.params.id + '.json'))
+
+    if(!req.params.id) return res.status(404).send('404');
+    try{
+        console.log(path.join('../../config/arts', req.params.id + '.json'))
+        data = require(path.join('../../config/arts', req.params.id + '.json'));
+    }
+    catch(err){
+        return res.status(404).send('404');
+    }
+
+    res.render('imp1/article', extend(data, public))
+})
 
 module.exports = router;
