c84ba84563d23ec7b70a54ff460cfff4ad198daf
gavinning <ningyubo@gmail.com>
2016-11-27 18:06:05 +0800

    v1.1.1 优化sms

--- src/main/node/models/system/sms.js
+++ src/main/node/models/system/sms.js
@@ -1,28 +1,22 @@
 var path = require('path');
+var format = require('date-format');
 var Alidayu = require('alidayu-sms');
 var clientOption = require(path.join(process.env.HOME, 'db/sms'));
 var smsOption = {
      'extend' : '' ,
      'sms_type' : 'normal' ,
      'sms_free_sign_name' : '长歌' ,
-     'sms_param' : "{name:'username', time: '0点'}" ,
+     'sms_param' : "{name:'username', time: '0点', username: '测试'}" ,
      'rec_num' : '13216969487' ,
-     'sms_template_code' : "SMS_27725104"
+     'sms_template_code' : "SMS_30000219"
 }
 var sms = new Alidayu(clientOption, smsOption);
 
 // PROD
-module.exports = function(timeline) {
+module.exports = function(username) {
     return sms.send(18910063857, {
         name: '老王',
-        time: timeline || (new Date()).toJSON().slice(11, 16)
+        time: format('hh:mm'),
+        username: username
     });
 }
-//
-// // DEVP
-// module.exports = function(timeline) {
-//     return sms.send(13216969487, {
-//         name: '叶长歌',
-//         time: timeline || (new Date()).toJSON().slice(11, 16)
-//     });
-// }
